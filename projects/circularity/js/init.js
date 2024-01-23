47bd92ea3397abcd5099de74c688cb9245eadf59
Kurt Hargis <kurtcharleshargis@gmail.com>
2024-01-23 18:58:01 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,26 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+            var circle;
+            var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+            function drawCircle()
+            {
+                // Code to draw a circle
+                circle = draw.randomCircleInArea(canvas, true, true, "#999", 10);
+                physikz.addRandomVelocity(circle, canvas, 8, 8);
+                view.addChild(circle);
+                circles.push(circle);
+                
+                
+            }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        for (var i = 0; i < 100; i++)
+        {
+         drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,11 +52,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            for(var i = 0; i< circles.length; i++)
+            {
+            physikz.updatePosition(circles[i]);
+            }
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            for(var i = 0; i< circles.length; i++)
+            {
+            game.checkCirclePosition(circles[i]);
+            }
+            
             // TODO 9 : Iterate over the array
            
             
@@ -61,10 +81,20 @@ var init = function (window) {
                 circle.x = 0;
             }
             
-            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
+            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+            if (circle.x < 0)
+            {
+                circle.x = canvas.width;
+            }
 
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+           }
 
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+           }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,16 +43,22 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
+    createPlatform(200, 230, 200, 20 );
+    createPlatform(1000, 500, 200, 20 );
+    createPlatform(600, 680, 100, 100 );
+    createPlatform(250, 400, 125, 125 );
+    createPlatform(1040, 643, 82, 82 );
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+   
+    createCollectable("max", 250, 150, 20, .75);
+    createCollectable("database", 750, 750, 20, .75);
+    createCollectable("grace", 150, 350, 20, .75);
+    createCollectable("kennedi", 450, 650, 20, .75);
 
 
 
@@ -61,7 +67,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("left", 300, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("right", 100, 500);
+    createCannon("top", 1000, 1500);
 
 
 
